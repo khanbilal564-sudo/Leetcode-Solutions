28203e540f4e8ab2a6858960fa1e766236eb39b3
khanbilal564-sudo <khan.bilal.564786@gmail.com>
2026-07-29 12:40:38 +0530

    Time: 3 ms (2.7%), Space: 14.3 MB (5.56%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-    bool check(vector<int>& a) {
-        int max=a[0];
-        for(int i=0;i<a.size();i++){
-            if(a[i]>=max) max=a[i];
-            else if(max==a[0]){
-                if(a[a.size()-1]==max){
-                    for(int j=i+1;j<a.size();j++){
-                    if(a[j]>=a[i] && a[j]>=a[j-1] && a[j]<=max) continue;
-                    else return false;
-                    }
-                }
-                else if(a[a.size()-1]<a[0]){
-                    for(int j=i+1;j<a.size();j++){
-                    if(a[j]>=a[i] && a[j]>=a[j-1] && a[j]<max) continue;
-                    else return false;
-                    }
-                }
-                else return false;
-            }
-            else{
-                if(a[a.size()-1]>a[0]) return false;
-                for(int j=i+1;j<a.size();j++){
-                    if(a[j]>=a[i] && a[j]>=a[j-1] && a[j]<max) continue;
-                    else return false;
-                }
+    bool check(vector<int>& arr){
+	for(int x=0;x<arr.size();x++){
+        vector<int>brr;
+		for(int i=0;i<arr.size();i++){
+			int ele=arr[(i+x)%arr.size()];
+            brr.push_back(ele);
+		}
+        int count=0;
+        for(int i=1;i<brr.size();i++){
+            if(brr[i]<brr[i-1]){
+                count++;
+                break;
             }
         }
-        return true;
+        if(count==0) return true;
     }
+    return false;
+}
 };
