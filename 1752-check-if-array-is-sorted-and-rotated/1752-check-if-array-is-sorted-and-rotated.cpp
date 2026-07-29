30024c71ea0a4c6076e5f98d28cdfce48ca95a14
khanbilal564-sudo <khan.bilal.564786@gmail.com>
2026-07-29 15:13:01 +0530

    Time: 0 ms (100%), Space: 11.3 MB (56.16%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     bool check(vector<int>& arr){
-	for(int x=0;x<arr.size();x++){
-        vector<int>brr;
-		for(int i=0;i<arr.size();i++){
-			int ele=arr[(i+x)%arr.size()];
-            brr.push_back(ele);
-		}
-        int count=0;
-        for(int i=1;i<brr.size();i++){
-            if(brr[i]<brr[i-1]){
-                count++;
-                break;
+    int n=arr.size();
+	for(int i=1;i<n;i++){
+        if(arr[i]<arr[i-1]){
+            int x=i;//the number of rotations
+            int temp[n];
+            for(int j=0;j<n;j++){
+                temp[j]=arr[(j+x)%n];
             }
+
+            for(int j=1;j<n;j++){
+                if(temp[j]<temp[j-1]){
+                    return false;
+                }
+            }    
         }
-        if(count==0) return true;
     }
-    return false;
+    return true;
 }
 };
