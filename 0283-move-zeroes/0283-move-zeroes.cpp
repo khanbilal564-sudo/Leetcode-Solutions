00cafd6929545a8ca78250250c1bd74b524c53bd
khanbilal564-sudo <khan.bilal.564786@gmail.com>
2026-08-02 20:36:56 +0530

    Time: 4 ms (10.54%), Space: 23.8 MB (83.4%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        if(nums.size()==0 || nums.size()==1) return;
-        int zero=0;
-        for(int i=0;i<nums.size();i++){                                                                             
-            if(nums[i]!=0){
-                nums[zero]=nums[i];
-                zero++;
-            }
+      if(nums.size()==0 || nums.size()==1) return;  
+      int i=-1;
+      for(int j=0;j<nums.size();j++){
+        if(nums[j]==0){
+            i=j;
+            break;
+        }
+      }
+      if(i==-1) return;
 
+      for(int j=i+1;j<nums.size();j++){
+        if(nums[j]!=0){
+            swap(nums[j],nums[i]);
+            i++;
         }
-        for(int i=zero;i<nums.size();i++){
-            if(nums[i]!=0) nums[i]=0;
-        }
+      }
         
     }
 };
