7891c312bd01b0756b223ba0b21f58f2e879984b
khanbilal564-sudo <khan.bilal.564786@gmail.com>
2026-08-03 23:27:02 +0530

    Time: 0 ms (100%), Space: 20.6 MB (59.33%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int>map;
-        for(auto it:nums){
-            map[it]++;
+        int result=0;
+        for(int i=0;i<nums.size();i++){
+            result^=nums[i];
         }
-        for(auto it:map){
-            if(it.second==1) return it.first;
-        }
-        return 0;
+        return result;
     }
 };
