c92e4fbba7c4c47064f9189f5c8a2f8c6f9a8ef0
khanbilal564-sudo <khan.bilal.564786@gmail.com>
2026-08-04 20:33:36 +0530

    Time: 119 ms (5.03%), Space: 14.1 MB (84.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,8 +6,7 @@ public:
     
         for(int i=0;i<n;i++){
             int sum=0;
-            for(int j=0;j<n;j++){
-                if(j==i) continue;
+            for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j]==target){
                     ans.push_back(i);
                     ans.push_back(j);
