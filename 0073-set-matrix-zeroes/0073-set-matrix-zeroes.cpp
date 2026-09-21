840c482a585c9ee15e19d9855cc82977a8a7dbbb
khanbilal564-sudo <khan.bilal.564786@gmail.com>
2026-09-21 23:34:50 +0530

    Time: 0 ms (100%), Space: 20.9 MB (81.83%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -17,10 +17,12 @@ public:
             }
         }
 
-        for(int i=m-1;i>=1;i--){
-            for(int j=n-1;j>=1;j--){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
-                    matrix[i][j]=0;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
                 }
             }
         }
