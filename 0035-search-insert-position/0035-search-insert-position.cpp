c08d5f68577a488ab63385cf3739ca1d26ba4f76
khanbilal564-sudo <khan.bilal.564786@gmail.com>
2026-09-26 18:16:57 +0530

    Time: 0 ms (100%), Space: 13.7 MB (6.26%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -7,14 +7,15 @@ public:
 
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid] == target) return mid;
-            if(nums[mid] < target){
-                if(mid > index) index=mid+1;
-                low=mid+1;
+            if(nums[mid] >= target ){
+                if(nums[mid] == target) return mid;
+                else{
+                    index=mid;
+                    high = mid-1;
+                }
             }
             else{
-                if(mid < index) index=mid;
-                high=mid-1;
+                low = mid+1;
             }
         }
         return index;
